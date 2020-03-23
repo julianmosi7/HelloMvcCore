2d1fb21efdd1c8978cc15e0128ea110710328604
Julian <moshammer.ju@gmail.com>
2020-03-23 16:01:30 +0100

    init

---
+++ HelloMvcCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloMvcCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
